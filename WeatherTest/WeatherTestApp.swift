48a2657d8d41ba69d90db98e2b9a7a116dc97156
Jan Hovland <jan.hovland@lyse.net>
2022-06-27 13:13:24 +0200

    Initial Commit

---
+++ WeatherTest/WeatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp.swift
+//  WeatherTest
+//
+//  Created by Jan Hovland on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
